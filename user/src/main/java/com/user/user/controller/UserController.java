c694eb98b9774d87d367e52c66eb83236f30f58f
jafeth001 <jafethadet2021@gmail.com>
2025-03-14 15:38:57 +0300

    IZ test

---
+++ user/src/main/java/com/user/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.user.user.controller;
+
+import com.user.user.entity.User;
+import com.user.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+}
---
+++ user/src/main/java/com/user/user/entity/User.java
@@ -0,0 +1,23 @@
+package com.user.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
